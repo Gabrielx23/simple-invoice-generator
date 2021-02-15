c800fe99d708e55f4aa50e85a288bf8fc025bd3a
gabriel <gabriel.slawski@gmail.com>
2021-02-15 15:49:44 +0100

    project init

---
+++ src/App/Enum/env-key.enum.ts
@@ -0,0 +1,12 @@
+export enum EnvKeyEnum {
+  JWTSecretKey = 'JWT_SECRET',
+  JWTRefreshSecretKey = 'JWT_REFRESH_SECRET',
+  JWTExpiresIn = 'JWT_EXPIRES_IN',
+  JWTExpiresInUnit = 'JWT_EXPIRES_IN_UNIT',
+  SWEndpoint = 'SW_ENDPOINT',
+  AppUrl = 'APP_URL',
+  SwaggerTitle = 'SWAGGER_TITLE',
+  SwaggerDescription = 'SWAGGER_DESCRIPTION',
+  SwaggerVersion = 'SWAGGER_VERSION',
+  SwaggerUri = 'SWAGGER_URI',
+}
